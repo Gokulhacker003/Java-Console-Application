3b14f489127f4d606c3be332043bb25d3ee0eb22
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2024-12-22 11:48:31 +0530

    Update Customer.java

--- ATM/Customer.java
+++ ATM/Customer.java
@@ -1,26 +1,56 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Customer{
-    private String Cus_ID;
-    private String Cus_pass;
-    private String Cus_Name;
-    public Customer(String New_Cus_ID, String New_Cus_pass, String New_Cus_name){
-        this.Cus_ID=New_Cus_ID;
-        this.Cus_Name=New_Cus_name;
-        this.Cus_pass=New_Cus_pass;
+    private static String CusID;
+    private static String CusPass;
+    private static String CusName;
+    private static final Scanner scanner=new Scanner(System.in);
+    private ArrayList<Customer> ACus=new ArrayList<>();
+    public Customer(String cusID, String cusPass, String cusName) {
+        CusID = cusID;
+        CusPass = cusPass;
+        CusName = cusName;
+    }
+    public Customer(){
+    }
+    public static void CusCheck(String CU_ID,String CU_PASS){
+        if(CU_ID.equals(CusID)){
+            int attempt=0;
+            while (attempt<3){
+                attempt++;
+                if (CU_PASS.equals((CusPass))) {
+                    System.out.println("Your are Login Successfully!");
+                    System.out.printf("Welcome %s!%n", "Cus_Name");
+                    Customer C =new Customer();
+                    System.out.println();
+                    break;
+                } else{
+                    System.out.printf("%d No of Attempts\n",attempt);
+                    System.out.println("Your Password is wrong Please enter your correct Password");
+                    System.out.println("Enter Your Password:");
+                    CU_PASS=scanner.nextLine();
+                    if(attempt==3){
+                        System.out.println("You try to login 3 times So \n \t Please Try to Login Later");
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            System.out.println("Sorry! User ID or Password is wrong");
+        }
+    }
+    @Override
+    public String toString() {
+        return String.format("Customer ID: %s, Customer Password: %s, Customer Name: %s", CusID, CusPass, CusName);
     }
 
-    public Customer() {
+    public void setACus(Customer ACus) {
+        this.ACus.add(ACus);
     }
 
-    public String getCus_ID() {
-        return Cus_ID;
-    }
-
-    public String getCus_Name() {
-        return Cus_Name;
-    }
-    public String getCus_pass(){
-        return Cus_pass;
+    public ArrayList<Customer> getACus() {
+        return ACus;
     }
 }
