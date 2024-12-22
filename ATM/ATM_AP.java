39eac06ff46ff8f4fb8112475990d063d8d1cc8e
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2024-12-22 11:48:00 +0530

    Update ATM_AP.java

--- ATM/ATM_AP.java
+++ ATM/ATM_AP.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ATM_AP {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-
         while(Admin.run){
             System.out.println("1.Customer");
             System.out.println("2.Admin");
@@ -12,7 +11,11 @@ public class ATM_AP {
             int choice= Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1 -> {
-                    System.out.println("Welcome");
+                    System.out.println("Enter the Your ID:");
+                    String Input_id = scanner.nextLine();
+                    System.out.println("Enter Your Password:");
+                    String Input_password = scanner.nextLine();
+                    Customer.CusCheck(Input_id, Input_password);
                 }
                 case 2 -> {
                     System.out.println("Enter the Your ID:");
@@ -25,9 +28,7 @@ public class ATM_AP {
                     Admin.run = false;
                     System.out.println("Exiting");
                 }
-                default -> {
-                    System.out.println("Invaild choice!");
-                }
+                default -> System.out.println("Invaild choice!");
             }
         }
     }
