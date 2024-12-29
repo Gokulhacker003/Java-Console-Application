b667587257fe623dfb4779aa95e8ccd8a82d3ccb
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2024-12-29 17:45:37 +0530

    Delete ATM_Console_updated directory

--- ATM_Console_updated/ATM_Main.java
+++
@@ -1,5 +0,0 @@
-public class ATM_Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        ATM_OP.start();
-    }
-}
--- ATM_Console_updated/Notes/Two_Hundred.java
+++
@@ -1,8 +0,0 @@
-package Notes;
-
-public class Two_Hundred extends Note {
-    public Two_Hundred(String note,int count){
-        this.setNote(note);
-        this.setCount(count);
-    }
-}
