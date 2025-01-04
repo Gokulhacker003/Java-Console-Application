2cc3b7c53bbb65e92c8c1bbe249e3f436312366a
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2025-01-04 17:05:03 +0530

    Delete ATM_Console1_UP/Notes/Two_Thousand.java

--- ATM_Console1_UP/Notes/Two_Thousand.java
+++
@@ -1,8 +0,0 @@
-package  Notes;
-   //class for Two Thousand note
-    public class Two_Thousand extends Note {
-        public Two_Thousand(String note, int count) {
-            this.setNote(note);  // Set note value
-            this.setCount(count); // Set note count
-        }
-    }
