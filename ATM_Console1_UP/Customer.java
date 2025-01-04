59d82dd78327d8819b809f813573171fcfa33b32
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2025-01-04 17:08:55 +0530

    Update Customer.java

--- ATM_Console1_UP/Customer.java
+++ ATM_Console1_UP/Customer.java
@@ -1,40 +1,16 @@
 import java.util.ArrayList;
 
 // Customer class to represent customer details and actions
-public class Customer {
-    private String uid; // Unique customer ID
-    private String pass; // Customer password
-    private String name; // Customer name
-    private long balance; // Customer account balance
-    private ArrayList<Transaction> transHistory; // Customer transaction history
+public class Customer extends Account {
+    private long balance;// Customer account balance
 
     // Constructor to initialize a customer with ID, password, and name
     public Customer(String ID, String pass, String name) {
-        this.uid = ID;
-        this.pass = pass;
-        this.name = name;
-        this.balance = 0; // Default balance is 0
-        transHistory = new ArrayList<>(); // Initialize transaction history
+        super(ID,pass,name);
     }
 
-    // Redeclare constructor
+    // Default constructor
     public Customer() {
-    }
-
-    public String getID() {
-        return uid;
-    }
-
-    public String getPassword() {
-        return pass;
-    }
-
-    public void setPassword(String pass) {
-        this.pass = pass;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public long getBalance() {
@@ -43,9 +19,5 @@ public class Customer {
 
     public void setBalance(long balance) {
         this.balance = balance;
-    }
-
-    public ArrayList<Transaction> getTransHistory() {
-        return transHistory;
     }
 }
