ae2b6e8f6e204418c66e8dc7d3c7d153559da806
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2025-01-04 17:04:32 +0530

    Delete ATM_Console1_UP/Notes/Hundred.java

--- ATM_Console1_UP/Notes/Hundred.java
+++
@@ -1,9 +0,0 @@
-package Notes;
-
-//class for Hundred note
-public class Hundred extends Note {
-    public Hundred(String note, int count) {
-        this.setNote(note);  // Set note value
-        this.setCount(count); // Set note count
-    }
-}
